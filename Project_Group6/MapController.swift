e70356fd1960dd2b579c1ccdc6332d9b088362db
Cynthia1211 <yue.cynthia.zhang@gmail.com>
2026-06-28 11:32:01 -0400

    Initial Commit

---
+++ Project_Group6/MapController.swift
@@ -0,0 +1,29 @@
+//
+//  MapController.swift
+//  Project_Group6
+//
+//  Created by 张玥 on 2026-06-28.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project_Group6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_Group6
+//
+//  Created by 张玥 on 2026-06-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
